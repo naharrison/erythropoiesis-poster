dc493dbdbac0ef3714aa1e191bd0f42f44d9dfc8
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2019-07-03 15:49:54 -0400

    Resizable window, better input file formatting

--- src/main/java/edu/ung/biol2130k/Poster.java
+++ src/main/java/edu/ung/biol2130k/Poster.java
@@ -19,10 +19,27 @@ public class Poster extends PApplet {
   public PImage flowchart;
   public Integer debugMode;
   public Txt2Data txt2data;
+  public Integer winw, winh; // window width, height
+  public float picWidthFrac;
 
 
   public void settings() {
-    size(1500, 975);
+    picWidthFrac = (float) 0.65;
+
+    winw = 400; // default values in case windowSize.txt DNE
+    winh = 400;
+
+    // read the window size from file:
+    BufferedReader reader = createReader("windowSize.txt");
+    try {
+      winw = Integer.parseInt(reader.readLine());
+      winh = Integer.parseInt(reader.readLine());
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    size(winw, winh);
   }
 
 
@@ -47,19 +64,19 @@ public class Poster extends PApplet {
 
   public void draw() {
     background(240);
-    image(flowchart, 0, 0, 975, 975);
+    image(flowchart, 0, 0, (int) (picWidthFrac*winw), winh);
 
     for(int k = 0; k < txt2data.x.size(); k++) {
-      int x = txt2data.x.get(k);
-      int y = txt2data.y.get(k);
-      int r = txt2data.r.get(k);
+      int x = (int) ((txt2data.x.get(k)/100.0)*picWidthFrac*winw);
+      int y = (int) ((txt2data.y.get(k)/100.0)*winh);
+      int r = (int) ((txt2data.r.get(k)/100.0)*picWidthFrac*winw);
 
       fill(0);
-      if(Math.sqrt((mouseX-x)*(mouseX-x) + (mouseY-y)*(mouseY-y)) < r) text(txt2data.info.get(k), 980, 0, 520, 975);
+      if(Math.sqrt((mouseX-x)*(mouseX-x) + (mouseY-y)*(mouseY-y)) < r) text(txt2data.info.get(k), (int) (picWidthFrac*winw)+10, 10, (int) (((1.0-picWidthFrac)*winw)-10), winh);
 
       if(debugMode.equals(1)) {
-        text(mouseX, 980, 955);
-        text(mouseY, 1025, 955);
+        text(  (int) (mouseX*100.0/(picWidthFrac*winw))  , (int) (picWidthFrac*winw)+10, winh-20);
+        text(  (int) (mouseY*100.0/winh)  , (int) (picWidthFrac*winw)+60, winh-20);
         fill(0, 204, 255, 70);
         ellipse(x, y, 2*r, 2*r);
       }
--- src/main/java/edu/ung/biol2130k/Txt2Data.java
+++ src/main/java/edu/ung/biol2130k/Txt2Data.java
@@ -6,14 +6,13 @@ import java.util.ArrayList;
 
 public class Txt2Data {
 
-  public ArrayList<Integer> x, y, r;
-  public ArrayList<String> name, info;
+  public ArrayList<Double> x, y, r;
+  public ArrayList<String> info;
 
   public Txt2Data(BufferedReader reader) {
     x = new ArrayList<>();
     y = new ArrayList<>();
     r = new ArrayList<>();
-    name = new ArrayList<>();
     info = new ArrayList<>();
 
     Integer itemLineNo = 0;
@@ -22,13 +21,12 @@ public class Txt2Data {
       String thisLine = reader.readLine();
       while(thisLine != null) {
         // itemLineNo == 0 are the separating "ITEM:" lines
-        if(itemLineNo.equals(1)) x.add(Integer.parseInt(thisLine));
-        else if(itemLineNo.equals(2)) y.add(Integer.parseInt(thisLine));
-        else if(itemLineNo.equals(3)) r.add(Integer.parseInt(thisLine));
-        else if(itemLineNo.equals(4)) name.add(thisLine);
-        else if(itemLineNo.equals(5)) info.add(thisLine);
+        if(itemLineNo.equals(1)) x.add(Double.parseDouble(thisLine));
+        else if(itemLineNo.equals(2)) y.add(Double.parseDouble(thisLine));
+        else if(itemLineNo.equals(3)) r.add(Double.parseDouble(thisLine));
+        else if(itemLineNo.equals(4)) info.add(thisLine);
         itemLineNo++;
-        itemLineNo = itemLineNo % 6;
+        itemLineNo = itemLineNo % 5;
         thisLine = reader.readLine();
       }
       reader.close();
